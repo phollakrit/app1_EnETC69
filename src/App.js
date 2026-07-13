736fe9b5f0fdf1dd5ac67d96871880457c9a1b53
pat123 <phollakrit@hotmail.com>
2026-07-13 11:05:33 +0700

    Update App.js with css inline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 
 function App() {
-    let r='React'
-    let rn='React Native'
-    const el=(
-      <div>
-        Create Web & Moblie Apps<br/>
-        with {r} & {rn}
-      </div>
-    )
-  return   el
+    const divStyle={
+      color:'red',
+      backgroundColor:'powderblue',
+      fontSize:'larger',
+      padding:'3px'
+    }
+  return  <div style={divStyle}>Hello React</div>
   
 }
 
