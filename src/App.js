71afd026912a4513d14d0346d21f2049caf04963
pat123 <phollakrit@hotmail.com>
2026-07-20 10:48:16 +0700

    function components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './style.css'
+import React from "react";
+import { Content, Footer, Header } from "./func-components";
+
 
 function App() {
  
   return  (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
+      <Header/>
+      <Content/>
+      <Footer/>
     </>
   )
   
