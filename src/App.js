3800cdbc65a1f8857ac092fdfee7aee1d7b0c761
pat123 <phollakrit@hotmail.com>
2026-08-03 10:09:24 +0700

    parameter in Event Handler

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { Content, Footer, Header } from "./func-components";
-import Calendar from "./class-components";
+import { Calculator2 } from "./calculator";
 
 
 function App() {
  
-  return  (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+  return <Calculator2/>
   
 }
 
