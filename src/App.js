c581b9297141cecb67cdadae3a0af763de3e4ef0
pat123 <phollakrit@hotmail.com>
2026-08-10 09:57:54 +0700

    Ref in function component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-
-import { EventData2 } from "./event-data";
+import RefsFunc from "./refs-func";
 
 
 function App() {
  
-  return <EventData2/>
+  return <RefsFunc/>
   
 }
 
