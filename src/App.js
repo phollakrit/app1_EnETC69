86d779adecdb81e9c240f233db68e952d781663e
pat123 <phollakrit@hotmail.com>
2026-08-24 11:03:33 +0700

    Context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-
+import {userContext} from './context'
+import Header2 from "./context-header2";
+import Content2 from "./context-content2";
 import Effect from "./effect";
 
 
 function App() {
+ let [user,setUser]=React.useState('')
  
-  return <Effect/>
+  return(
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+  )
   
 }
 
